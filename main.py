b8ed85a9a1ce3a99cd3d96e3094d5152bdf9f27b
Chinmaya <g.chinmaya32@gmail.com>
2023-05-31 17:40:13 +0530

    Data File Added

---
+++ main.py
@@ -0,0 +1,3 @@
+from scr.textSummarizer.logging import logger
+
+logger.info("Welcome to out custom log")
--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-
-
-
-
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer"
